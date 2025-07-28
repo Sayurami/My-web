2cfbad5c0a7256245910c5dd36959932743fc834
Sayurami <sayuramihiranga4@gmail.com>
2025-07-28 21:10:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const api = require("./api");
+const gojo = require("./gojo");
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'html')));
